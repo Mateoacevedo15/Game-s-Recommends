98218534de26bb197882454a6ac644eb260d6ffb
Mateoacevedo15 <mateo.acevedom.col@upb.edu.co>
2023-10-09 13:42:12 -0500

    Se termino con la interfaz de menu principal, se crea la clase RecoPage

---
+++ app/lib/Reco_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RecoPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Página de Solicitud de Recomendación'),
+      ),
+      body: Center(
+        child: Text('Contenido de la página de solicitud de recomendación'),
+      ),
+    );
+  }
+}
--- app/lib/menu_page.dart
+++ app/lib/menu_page.dart
@@ -1,66 +1,154 @@
 import 'package:flutter/material.dart';
-
+import 'Reco_page.dart';
 class MenuPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF6E168D), // Fondo igual que la primera pantalla
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            // Recuadro azul en la parte superior
-            Container(
-              color: Colors.blue,
-              width: double.infinity, // Ocupa todo el ancho
-              height: 100, // Altura fija
-              alignment: Alignment.center,
-              child: Text(
-                'Menu Principal',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
+      backgroundColor: const Color(0xFF6F73D2), // Fondo igual que la primera pantalla
+      appBar: AppBar(
+        title: const Text("Menú Principal"),
+        backgroundColor: Color.fromARGB(255, 25, 75, 184),
+        toolbarHeight: 40,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const SizedBox(
+            height: 20,
+          ),
+          const Text(
+            "Game's Recommend",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Color(0xff52d1dc),
+              fontFamily: 'Roboto',
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          // Primera fila de fotos y botones
+          Row(
+            children: [
+              const SizedBox(
+                width: 10,
+              ),
+              Expanded(
+                child: Column(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(
+                          10.0), // Ajusta el valor para redondear más o menos
+                      child: Image.asset('assets/jugados.png'),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        // Acción cuando se presiona el botón 1
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xffEF476F),
+                        foregroundColor:
+                            const Color(0xff52d1dc), // Color del texto
+                      ),
+                      child: const Text("Ver jugados"),
+                    ),
+                  ],
                 ),
               ),
-            ),
-            // Espacio entre el recuadro azul y los cuadros
-            SizedBox(height: 20),
-            // Cuadros naranja y verde alineados horizontalmente
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  width: 150,
-                  height: 200,
-                  color: Colors.orange, // Color del primer cuadro
-                ),
-                SizedBox(width: 20), // Espacio entre los cuadros
-                Container(
-                  width: 150,
-                  height: 200,
-                  color: Colors.green, // Color del segundo cuadro
-                ),
-              ],
-            ),
-            // Espacio entre los cuadros y el tercer cuadro
-            SizedBox(height: 20),
-            // Tercer cuadro centrado debajo de los dos cuadros
-            Center(
-              child: Container(
-                width: 300,
-                height: 200,
-                color: Colors.blue, // Color del tercer cuadro
+              const SizedBox(
+                width: 10,
               ),
-            ),
-            // Espacio para centrar verticalmente en la mitad de la pantalla
-            Expanded(child: Container()),
-          ],
-        ),
+              Expanded(
+                child: Column(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(
+                          10.0), // Ajusta el valor para redondear más o menos
+                      child: Image.asset('assets/pendientes.png'),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        // Acción cuando se presiona el botón 2
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xffEF476F),
+                        foregroundColor:
+                            const Color(0xff52d1dc), // Color del texto
+                      ),
+                      child: const Text("Ver pendientes"),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+            ],
+          ),
+          const SizedBox(height: 20), // Espacio entre filas
+          // Segunda fila de foto y botón
+          Column(
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: 150,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20.0),
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                      right: 20,
+                      left: 20,
+                      bottom: 10,
+                    ),
+                    child: ClipRRect(
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(20.0)),
+                      child: Image.asset(
+                        'assets/reco.png',
+                        fit: BoxFit.cover,
+                        width: MediaQuery.of(context).size.width,
+                        height: 250,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => RecoPage(),
+                    ),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xffEF476F),
+                  foregroundColor:
+                      const Color(0xff52d1dc), // Color del texto
+                ),
+                child: const Text("Solicitar Recomendación"),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
 }
+
+
+
+
 
 
 
